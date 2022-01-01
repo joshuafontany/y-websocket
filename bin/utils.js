604807d117cd174df18477a57b157ff62019c321
Joshua Fontany <joshua.fontany@gmail.com>
2021-12-31 22:28:31 -0800

    emit load on node sync from leveldb

--- bin/utils.js
+++ bin/utils.js
@@ -42,6 +42,7 @@ if (typeof persistenceDir === 'string') {
       ydoc.on('update', update => {
         ldb.storeUpdate(docName, update)
       })
+      ydoc.emit('load')
     },
     writeState: async (docName, ydoc) => {}
   }
